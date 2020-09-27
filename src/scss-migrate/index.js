cdaa6b54b8c7275f7530adf230842011dec05bcb
Thabo <ngubanethabo.ambrose@gmail.com>
2020-09-27 20:37:31 +0200

    Schematic done

--- src/scss-migrate/index.js
+++ src/scss-migrate/index.js
@@ -3,9 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.scssMigrate = void 0;
 const schematics_1 = require("@angular-devkit/schematics");
 const project_1 = require("@schematics/angular/utility/project");
-const parse_name_1 = require("@schematics/angular/utility/parse-name");
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
 function scssMigrate(_options) {
     return (tree, _context) => {
         const glob = require("glob");
@@ -13,27 +10,60 @@ function scssMigrate(_options) {
         if (!workspaceConfigBuffer) {
             throw new schematics_1.SchematicsException('Not an Angular CLI project');
         }
-        const workspaceConfig = JSON.parse(workspaceConfigBuffer.toString());
-        const projectName = workspaceConfig.defaultProject;
-        const project = workspaceConfig.projects[projectName];
-        const defaultProjectPath = project_1.buildDefaultPath(project);
-        const parsedPath = parse_name_1.parseName(defaultProjectPath, _options.name);
-        const { path } = parsedPath;
-        let filePaths = glob.sync(`.${path}/**/*.${_options.from}`);
-        console.log('Files to rename', filePaths);
-        filePaths.forEach(filePath => {
-            let content;
-            let filePathNoExtension = filePath.substr(0, filePath.lastIndexOf('.'));
-            let fileName = filePathNoExtension.substr(filePathNoExtension.lastIndexOf('/') + 1, filePathNoExtension.length);
-            let newFilePath = `${filePathNoExtension}.${_options.to}`;
-            tree.rename(filePath, newFilePath);
-            content = tree.exists(`${filePathNoExtension}.ts`) ? tree.read(`${filePathNoExtension}.ts`) : null;
-            if (content) {
-                const strContent = content.toString();
-                const finalstr = strContent === null || strContent === void 0 ? void 0 : strContent.replace(`${fileName}.${_options.from}`, `${fileName}.${_options.to}`);
-                tree.overwrite(`${filePathNoExtension}.ts`, finalstr);
+        else {
+            const shell = require('shelljs');
+            const workspaceConfig = JSON.parse(workspaceConfigBuffer.toString());
+            const projectName = workspaceConfig.defaultProject;
+            const project = workspaceConfig.projects[projectName];
+            // Needs improvement, maybe use shelljs.exec('ng config schematics.@schematics/angular:component.style scss')?
+            // Maybe its possible to use RunSchematicTask from '@angular-devkit/schematics/tasks'? To achieve  
+            // adding the new style schematic?
+            const workspaceSchematics = project ? project.schematics ? project.schematics : null : undefined;
+            if (workspaceSchematics === undefined) {
+                throw new schematics_1.SchematicsException('Not a valid Angular CLI project');
             }
-        });
+            if (workspaceSchematics) {
+                let componectSchematics = workspaceSchematics['@schematics/angular:component'];
+                if (componectSchematics) {
+                    let styleSheetFormat = componectSchematics.style;
+                    componectSchematics.styleExt && delete componectSchematics.styleExt;
+                    if (styleSheetFormat) {
+                        styleSheetFormat = styleSheetFormat = 'scss';
+                    }
+                    else {
+                        componectSchematics.style = 'scss';
+                    }
+                }
+                else {
+                    workspaceSchematics['@schematics/angular:component'] = {
+                        "style": "scss"
+                    };
+                }
+            }
+            else {
+                project.schematics = { ['@schematics/angular:component']: {} };
+                project.schematics['@schematics/angular:component'] = {
+                    "style": "scss"
+                };
+            }
+            tree.overwrite('/angular.json', JSON.stringify(workspaceConfig, null, "\t"));
+            const defaultProjectPath = project_1.buildDefaultPath(project);
+            const filePaths = glob.sync(`.${defaultProjectPath}/**/*.${_options.from}`);
+            console.log('Files to rename', filePaths);
+            filePaths.forEach((filePath) => {
+                let relativeComponentClassFileContent;
+                let filePathNoExtension = filePath.substr(0, filePath.lastIndexOf('.'));
+                let fileName = filePathNoExtension.substr(filePathNoExtension.lastIndexOf('/') + 1, filePathNoExtension.length);
+                let newFilePath = `${filePathNoExtension}.${_options.to}`;
+                tree.rename(filePath, newFilePath);
+                relativeComponentClassFileContent = tree.exists(`${filePathNoExtension}.ts`) ? tree.read(`${filePathNoExtension}.ts`) : null;
+                if (relativeComponentClassFileContent) {
+                    const relativeComponentClassFileContentAsString = relativeComponentClassFileContent.toString();
+                    const finalComponentClassFileContent = relativeComponentClassFileContentAsString === null || relativeComponentClassFileContentAsString === void 0 ? void 0 : relativeComponentClassFileContentAsString.replace(`${fileName}.${_options.from}`, `${fileName}.${_options.to}`);
+                    tree.overwrite(`${filePathNoExtension}.ts`, finalComponentClassFileContent);
+                }
+            });
+        }
         return tree;
     };
 }
--- src/scss-migrate/index.ts
+++ src/scss-migrate/index.ts
@@ -1,13 +1,8 @@
-import { Rule, SchematicContext, Tree, url, apply, template, mergeWith, SchematicsException } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, SchematicsException } from '@angular-devkit/schematics';
 import { buildDefaultPath } from '@schematics/angular/utility/project'
-import { parseName } from '@schematics/angular/utility/parse-name'
 import { Schema } from './schema';
+import { RunSchematicTask } from '@angular-devkit/schematics/tasks';
 
-import { strings } from '@angular-devkit/core';
-
-
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
 export function scssMigrate(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     const glob = require("glob");
@@ -15,39 +10,77 @@ export function scssMigrate(_options: Schema): Rule {
 
     if (!workspaceConfigBuffer) {
       throw new SchematicsException('Not an Angular CLI project')
-    }
+    } else {
+      const shell = require('shelljs');
 
+      const workspaceConfig = JSON.parse(workspaceConfigBuffer.toString());
+      const projectName = workspaceConfig.defaultProject;
+      const project = workspaceConfig.projects[projectName];
 
-    const workspaceConfig = JSON.parse(workspaceConfigBuffer.toString());
-    const projectName = workspaceConfig.defaultProject;
-    const project = workspaceConfig.projects[projectName];
-    const defaultProjectPath = buildDefaultPath(project);
-    const parsedPath = parseName(defaultProjectPath, _options.name);
-    const { path } = parsedPath;
+      // Needs improvement, maybe use shelljs.exec('ng config schematics.@schematics/angular:component.style scss')?
+      // Maybe its possible to use RunSchematicTask from '@angular-devkit/schematics/tasks'? To achieve  
+      // adding the new style schematic?
 
+      const workspaceSchematics = project ? project.schematics ? project.schematics : null : undefined;
 
-    let filePaths = glob.sync(`.${path}/**/*.${_options.from}`);
-
-    console.log('Files to rename', filePaths);
-
-    filePaths.forEach(filePath => {
-      let content: Buffer;
-      let filePathNoExtension: string = filePath.substr(0, filePath.lastIndexOf('.'));
-      let fileName = filePathNoExtension.substr(filePathNoExtension.lastIndexOf('/') + 1, filePathNoExtension.length)
-      let newFilePath = `${filePathNoExtension}.${_options.to}`;
-
-      tree.rename(filePath, newFilePath);
-
-      content = tree.exists(`${filePathNoExtension}.ts`) ? tree.read(`${filePathNoExtension}.ts`) : null;
-
-      if (content) {
-        const strContent = content.toString();
-        const finalstr: string = strContent?.replace(`${fileName}.${_options.from}`, `${fileName}.${_options.to}`);
-
-        tree.overwrite(`${filePathNoExtension}.ts`, finalstr);
+      if (workspaceSchematics === undefined) {
+        throw new SchematicsException('Not a valid Angular CLI project')
       }
-    });
 
+      if (workspaceSchematics) {
+        let componectSchematics = workspaceSchematics['@schematics/angular:component'];
+
+        if (componectSchematics) {
+          let styleSheetFormat = componectSchematics.style;
+
+          componectSchematics.styleExt && delete componectSchematics.styleExt;
+
+          if (styleSheetFormat) {
+            styleSheetFormat = styleSheetFormat = 'scss';
+          } else {
+            componectSchematics.style = 'scss';
+          }
+        } else {
+          workspaceSchematics['@schematics/angular:component'] = {
+            "style": "scss"
+          };
+        }
+      } else {
+        project.schematics = { ['@schematics/angular:component']: {} };
+
+        project.schematics['@schematics/angular:component'] = {
+          "style": "scss"
+        };
+      }
+
+      tree.overwrite('/angular.json', JSON.stringify(workspaceConfig, null, "\t"));
+
+      const defaultProjectPath = buildDefaultPath(project);
+      const filePaths = glob.sync(`.${defaultProjectPath}/**/*.${_options.from}`);
+
+      console.log('Files to rename', filePaths);
+
+      filePaths.forEach((filePath: string) => {
+        let relativeComponentClassFileContent: Buffer;
+        let filePathNoExtension: string = filePath.substr(0, filePath.lastIndexOf('.'));
+        let fileName: string = filePathNoExtension.substr(filePathNoExtension.lastIndexOf('/') + 1, filePathNoExtension.length)
+        let newFilePath: string = `${filePathNoExtension}.${_options.to}`;
+
+        tree.rename(filePath, newFilePath);
+
+        relativeComponentClassFileContent = tree.exists(`${filePathNoExtension}.ts`) ? tree.read(`${filePathNoExtension}.ts`) : null;
+
+        if (relativeComponentClassFileContent) {
+          const relativeComponentClassFileContentAsString = relativeComponentClassFileContent.toString();
+          const finalComponentClassFileContent: string = relativeComponentClassFileContentAsString?.replace(
+            `${fileName}.${_options.from}`,
+            `${fileName}.${_options.to}`
+          );
+
+          tree.overwrite(`${filePathNoExtension}.ts`, finalComponentClassFileContent);
+        }
+      });
+    }
 
     return tree;
   };
